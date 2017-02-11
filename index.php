b0cf0e842428def3366121c9b9f36335a9b8449b
novidaws <novida.wayan@ui.ac.id>
2017-02-11 05:19:13 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?
+	$serverName = "localhost";
+	$userName = "root";
+	$password = "";
+	$dbName = "sbmptn_normalized";
+
+	$conn = mysql_connect($serverName, $userName, $password);
+	mysql_select_db($dbName, $conn);
+
+	// Check connection
+	//if ($conn->connect_error) {
+	//    die("Connection failed: " . $conn->connect_error);
+	//}else{
+		$nomorPeserta = $_GET["nomor"];
+		$sql = "select a.nomor, case when b.nomor is NULL then 0 else 1 end isLulus, a.nama, ";
+		$sql .= "a.tgl_lahir, b.kode_prodi, b.kode_univ, c.nama_prodi, d.nama_univ ";
+		$sql .= "from peserta a ";
+		$sql .= "left join result b on a.nomor = b.nomor ";
+		$sql .= "left join prodi c on b.kode_prodi = c.kode_prodi ";
+		$sql .= "left join univ d on b.kode_univ = d.kode_univ where a.nomor = '".$nomorPeserta."'";
+	
+//echo $sql;
+		$result = mysql_query($sql);
+		if (mysql_num_rows($result) > 0) {
+		    // output data of each row
+		    while($row = mysql_fetch_array($result)) {
+			echo "Nomor: ".$row["nomor"]."<br>";
+			echo "Nama: ".$row["nama"]."<br>";
+			echo "Tanggal Lahir: ".$row["tgl_lahir"]."<br>";
+			if($row["isLulus"] == 0){
+				echo "Maaf, anda tidak lulus SBMPTN.<br>";
+			}else{
+				echo "Selamat! Anda lulus SBMPTN<br>";
+				echo "Pada Program Studi ".$row["nama_prodi"]." ".$row["nama_univ"]."<br>";
+			}
+		    }
+		} else {
+		    echo "<i>Nomor peserta tidak terdaftar.</i>";
+		}
+		
+
+	//}
+	
+	
+?>
